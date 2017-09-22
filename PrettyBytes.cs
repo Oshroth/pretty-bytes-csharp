b7915fb5c433677204b65942c1cfe58b51954cfb
Tim Plester <tplester@deloitte.com.au>
2017-09-22 11:51:55 +1000

    Ported class to C#

---
+++ PrettyBytes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Utility {
+	public static class PrettyBytes {
+
+		static readonly string[] UNITS = { "B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+		public static string FormatBytes(double num) {
+			if (double.IsInfinity(num) || double.IsNaN(num)) {
+				throw new NotFiniteNumberException(num);
+			}
+
+			var neg = num < 0;
+
+			if (neg) {
+				num = -num;
+			}
+
+			if (num < 1) {
+				return (neg ? "-" : "") + num + " B";
+			}
+
+			var exponent = Math.Min((int)Math.Floor(Math.Log10(num) / 3), UNITS.Length - 1);
+			var numStr = Math.Round(num / Math.Pow(1000, exponent), 3);
+			var unit = UNITS[exponent];
+
+			return (neg ? "-" : "") + numStr + " " + unit;
+		}
+	}
+}
